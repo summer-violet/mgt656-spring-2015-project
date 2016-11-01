6858c06db0115900a9cdb9956ac50a070b425b6f
Raul Cisneros <raul.cisneros@yale.edu>
2016-11-01 23:30:01 +0000

    Hide old events on the homepage

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,14 @@ function index (request, response) {
   var contextData = {
     'title': 'MGT 656',
     'tagline': 'You are doomed (just kidding).'
+    'events': [] 
   };
+  for(var i=0; i < events.all.length; i++){
+    var event = events.all[i];
+    if(event.date > now){
+      contextData.events.push(event);
+    }
+  }
   response.render('index.html', contextData);
 }
 
