52880e9b83b266361cda745f3032e99e056c129d
Claudia Navarrete <claudia.navarrete@yale.edu>
2016-11-02 01:24:34 +0000

    merging events

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ var events = require('../models/events');
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date();
   var contextData = {
     'title': 'MGT 656',
     'tagline': 'You are doomed (just kidding).',
